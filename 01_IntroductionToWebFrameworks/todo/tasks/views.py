dfed52d30a8f1bb764e0ed51b4896e421d027e34
temcha93 <84852530+temcha93@users.noreply.github.com>
2021-09-08 15:22:34 +0600

    first lesson

--- 01_IntroductionToWebFrameworks/todo/tasks/views.py
+++ 01_IntroductionToWebFrameworks/todo/tasks/views.py
@@ -1,15 +1,21 @@
 from django.http import HttpResponse
-
+from random import shuffle
 from django.views import View
 
 
 class ToDoView(View):
 
     def get(self, request, *args, **kwargs):
-        return HttpResponse('<ol>'
-                            '<li>Установить python, выполнено</li>'
-                            '<li>Установить django, сделал</li>'
-                            '<li>Запустить сервер, все работает</li>'
-                            '<li>Порадоваться результату, рад</li>'
-                            '<li>Мне кажется у меня лишняя папка создалась в репозитории</li>'
-                            '</ol>')
+
+
+        list = ['Установить python, выполнено',
+                'Установить django, сделал',
+                'Запустить сервер, все работает',
+                'Порадоваться результату, рад'
+                ]
+        shuffle(list)
+        items = ''
+        for s in list:
+            items += f'<li>{s}</li>'
+        result = '<html><body><ol>%s</ol></body></html>' % items
+        return HttpResponse(result)
