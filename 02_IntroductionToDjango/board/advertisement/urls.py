7707639002f88ec625090700623fd4915a59766e
temcha <artem_oparin@list.ru>
2022-03-15 10:08:49 +0600

    DZ_2

--- 02_IntroductionToDjango/board/advertisement/urls.py
+++ 02_IntroductionToDjango/board/advertisement/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.advertisement_list, name='advertisement_list')
+    path('', views.advertisement_list, name='advertisement_list'),
+    path('advertisement1/', views.advertisement1, name='advertisement1')
 
 ]
--- 02_IntroductionToDjango/board/advertisement/views.py
+++ 02_IntroductionToDjango/board/advertisement/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def advertisement_list(request, *args, **kwargs):
     return render(request, 'advertisement/advertisement_list.html', {})
+
+
+def advertisement1(request, *args, **kwargs):
+    return render(request, 'advertisement/advertisement1.html', {})
