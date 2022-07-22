1ff3e7b3221f1e89faca55db832b1cc833caa75b
temcha <artem_oparin@list.ru>
2022-07-22 11:49:07 +0600

    Homework for module 5

--- 04_DatabasesAndModels/board/advertisements_app/urls.py
+++ 04_DatabasesAndModels/board/advertisements_app/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from . import views
 
-urlpatterns = [
+urlpatterns = [path('', views.About.as_view(), name='advertisement'),
+               path('advertisements/', views.AdvertisementListView.as_view(), name='advertisement'),
+               path('about/', views.About.as_view()),
+               path('advertisements/<int:pk>', views.AdvertisementDetailView.as_view(),
+                    name='advertisement-detail')
 
 ]
--- 04_DatabasesAndModels/board/advertisements_app/views.py
+++ 04_DatabasesAndModels/board/advertisements_app/views.py
@@ -1,1 +1,33 @@
 from django.shortcuts import render
+from django.views import generic
+from django.views.generic import TemplateView
+from .models import Advertisement
+
+
+class About(TemplateView):
+    template_name = 'templates/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Бесплатные объявления'
+        context['name'] = 'ООО "Пайтон адвертисментс"'
+        context['description'] = """Расскажите о себе. Найдите поставщиков. Расширьте связи. 
+        Сосредоточьтесь на бизнесе. Все необходимые инструменты доступны 
+        сразу после регистрации."""
+        return context
+
+
+class AdvertisementListView(generic.ListView):
+    model = Advertisement
+    template_name = 'advertisement_list.html'
+    context_object_name = 'advertisement_list'
+
+
+class AdvertisementDetailView(generic.DetailView):
+    model = Advertisement
+
+    def get_object(self, queryset=None):
+        item = super().get_object(queryset)
+        item.views_count += 1
+        item.save()
+        return item
